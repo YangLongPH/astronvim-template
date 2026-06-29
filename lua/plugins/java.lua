0aec07272594854da5cf872de31d58ddb5345bb1
YangLong <yanglong.ph@gmail.com>
2026-06-29 09:32:39 +0700

    feat: add config nvim-jdtls use jave 21

---
+++ lua/plugins/java.lua
@@ -0,0 +1,16 @@
+---@type LazySpec
+return {
+  {
+    "mfussenegger/nvim-jdtls",
+    opts = function(_, opts)
+      local java21 = vim.env.JDTLS_JAVA
+      if java21 then
+        opts.cmd = {
+          vim.fn.stdpath "data" .. "/mason/bin/jdtls",
+          "--java-executable", java21,
+        }
+      end
+      return opts
+    end,
+  },
+}
